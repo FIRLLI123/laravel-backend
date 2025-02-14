7be92caeb443c624ecfa16a52873465b44e54ae8
FIRLLI123 <firlliantonizi@gmail.com>
2025-02-14 15:43:29 +0700

    update delete

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,5 @@ Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
 Route::get('/users/search', [UserController::class, 'searchByName']);
 Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
 //tes
